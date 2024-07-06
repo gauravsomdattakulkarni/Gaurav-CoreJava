930354563f7eeb8f4ec4583c74a788afed4e2774
Gaurav Somdatta Kulkarni <97786330+gauravsomdattakulkarni@users.noreply.github.com>
2024-07-06 07:36:44 +0530

    java programs : core java learning

---
+++ ConditionalStatement.java
@@ -0,0 +1,17 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int num = 12;
+        if(num==0)
+        {
+            System.out.println("The Number Is Zero");
+        }
+        else if(num%2==0)
+        {
+            System.out.println("The Number Is Even");
+        }
+        else
+        {
+            System.out.println("The Number Is Odd");
+        }
+    }
+}
